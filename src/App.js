9d4601607130652d5616b0d5f731355f8cc4540d
Leandro Bataglia <leandro.bataglia@gmail.com>
2023-02-08 16:56:06 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+
+async function getDados() {
+  let response = await axios.get(
+    "http://www.ipeadata.gov.br/api/odata4/Metadados('PRECOS_INPCBR')/Valores"
+  );
+
+  let data = await response.data.value.map((item) => {
+    const container = {
+      ano: new Date(item.VALDATA).getFullYear(),
+      valor: item.VALVALOR
+    };
+    return container;
+  }
+  );
+  console.log(data)
+};
+
+const dados = getDados();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Quanto valeria hoje</h1>
+      <form name="">
+        <input type='text' placeholder='insira o valor' />
+        <input type='text' placeholder='insira o ano' />
+        <button type='submit'>Submeter</button>
+      </form>
     </div>
   );
 }
