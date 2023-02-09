879dcd1894314552292434113f86972ce6036315
Leandro Bataglia <leandro.bataglia@gmail.com>
2023-02-09 14:54:54 -0300

    adicionando fetch async com a API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-async function getDados(){
-  let response = await axios.get(
-    "http://www.ipeadata.gov.br/api/odata4/Metadados('PRECOS_INPCBR')/Valores"
-  );
-
-  let data = await response.data.value.map((item) => {
-    const container = {
-        ano: new Date(item.VALDATA).getFullYear(),
-        valor: item.VALVALOR
-      };
-      return container;
-    }
-  );
-  return data;
-};
 
 function App() {
+
   return (
     <div className='App'>
       <h1>Quanto valeria hoje</h1>
@@ -32,27 +18,50 @@ function App() {
   );
 }
 
-export function DataList() {
-  let data = getDados().then((response) => {
 
-    
-  });
- 
+export function DataList() {
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    getDados();
+  }, []);
+
+  async function getDados(){
+    const response = await axios.get(
+      "http://www.ipeadata.gov.br/api/odata4/Metadados('PRECOS_INPCBR')/Valores"
+    );
+
+    const responseData = await response.data.value.map((item) => {
+      const container = {
+          ano: new Date(item.VALDATA).getFullYear(),
+          valor: item.VALVALOR
+        };
+        return container;
+      }
+    );
+    // console.log(responseData)
+    setData(responseData);
+  };
+  
+  const dados = [
+    {ano:2000, value:145286},
+    {ano:2001, value:145287}
+  ]
+
   return (
     <div className='DataList'>
       <ul>
-        {data.map((item) => (
-          <DataListItem dataYear={item.ano} dataValue={data.value} />
-        ))}
+        { data.map((item, index) => ( <DataListItem key={index} dataYear={item.ano} dataValue={item.valor} /> )) }
       </ul>
     </div>
-  )
+  );
 }
 
 export function DataListItem(props) {
   return (
     <div className='DataListItem'>
-      <li>{props.dataYear} {props.dataValue}</li>
+      <li>{props.dataYear} / {props.dataValue}</li>
     </div>
   )
 }
