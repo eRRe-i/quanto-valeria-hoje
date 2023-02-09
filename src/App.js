1dc5f612e383560a1cab80bf8ef0530dc481c807
Leandro Bataglia <leandro.bataglia@gmail.com>
2023-02-09 05:05:33 -0300

    componentes auxiliares adicionados

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
 
-async function getDados() {
+async function getDados(){
   let response = await axios.get(
     "http://www.ipeadata.gov.br/api/odata4/Metadados('PRECOS_INPCBR')/Valores"
   );
 
   let data = await response.data.value.map((item) => {
     const container = {
-      ano: new Date(item.VALDATA).getFullYear(),
-      valor: item.VALVALOR
-    };
-    return container;
-  }
+        ano: new Date(item.VALDATA).getFullYear(),
+        valor: item.VALVALOR
+      };
+      return container;
+    }
   );
-  console.log(data)
+  return data;
 };
-
-const dados = getDados();
 
 function App() {
   return (
@@ -29,8 +27,34 @@ function App() {
         <input type='text' placeholder='insira o ano' />
         <button type='submit'>Submeter</button>
       </form>
+      <DataList />
     </div>
   );
+}
+
+export function DataList() {
+  let data = getDados().then((response) => {
+
+    
+  });
+ 
+  return (
+    <div className='DataList'>
+      <ul>
+        {data.map((item) => (
+          <DataListItem dataYear={item.ano} dataValue={data.value} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export function DataListItem(props) {
+  return (
+    <div className='DataListItem'>
+      <li>{props.dataYear} {props.dataValue}</li>
+    </div>
+  )
 }
 
 export default App;
